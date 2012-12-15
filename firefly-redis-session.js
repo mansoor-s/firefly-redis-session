5b3d8b17983da999c1e4ba57f9d151e73a92f148
Mansoor Sayed <mansoor@mansoorsayed.com>
2012-12-14 19:50:46 -0500

    License

--- firefly-redis-session.js
+++ firefly-redis-session.js
@@ -1,5 +1,5 @@
 /*
-    Firefly - Node.js CMS
+    Firefly-Redis-Session A Redis based session manager for Firefly 
     Copyright (C) <2012>  <Mansoor Sayed>
 
     This program is free software: you can redistribute it and/or modify
